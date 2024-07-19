baf07b9b699f0120b8ca4e54a777362278cb94e5
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-19 15:59:37 +0300

    Add files via upload

--- MOD-8_module_8_2.py
+++ MOD-8_module_8_2.py
@@ -16,21 +16,31 @@ def personal_sum(numbers):
 
 def calculate_average(numbers):
     try:
+        num = []
+
+        for number in numbers:
+            if isinstance(number, (int, float)):
+                num.append(number)
+            else:
+                print(f'Некорректный тип данных для подсчёта суммы: {number}')
+
         summ = sum(personal_sum(numbers))
-        total = summ / len(numbers)
+        total = summ / len(num)
         return total
-    except ZeroDivisionError:
-        print("Список пуст, среднее значение не может быть вычислено.")
+
+    except ZeroDivisionError as z:
+        print(f'Список пуст, среднее значение не может быть вычислено: {z}')
+        return 0
+    except TypeError as t:
+        print(f'В numbers записан некорректный тип данных: {t}')
         return None
-    except TypeError:
-        print("В numbers записан некорректный тип данных.")
-        return None
 
 
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
 
 
